294b1b7064694afda42ed90960f9ac3203738892
benhsampson <bensampsondev@gmail.com>
2019-11-09 20:49:16 +1100

    added react hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Coordinates from "./Components/Coordinates";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Coordinates />
       </header>
     </div>
   );
