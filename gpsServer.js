fda505da61cab94e22abc45d584201c7221c91e5
benhsampson <bensampsondev@gmail.com>
2019-11-10 18:03:50 +1100

    add coord

--- gpsServer.js
+++ gpsServer.js
@@ -9,27 +9,30 @@ port.pipe(parser);
 
 var GPS = require("gps");
 var gps = new GPS();
+var coord = 0;
 
 const IOport = 8000;
 io.listen(IOport);
 console.log("gpsServer listening on port ", IOport);
+
+gps.on("data", function() {
+  if (gps.state.lat !== null) {
+    console.log("lat: " + gps.state.lat + " lon: " + gps.state.lon);
+    var coord = { lat: gps.state.lat, lon: gps.state.lon };
+    console.log(coord.lat, " ", coord.lon);
+  }
+});
+
+parser.on("data", function(data) {
+  gps.update(data);
+});
 
 io.on("connection", client => {
   client.on("subscribeToGPS", interval => {
     console.log("client is subscribing to GPS with interval ", interval);
     console.log("lat only", gps.state.lat);
     setInterval(() => {
-      client.emit("gps", gps.state);
+      client.emit("gps", coord);
     }, interval);
   });
-});
-
-gps.on("data", function() {
-  if (gps.state.lat !== null) {
-    console.log("lat: " + gps.state.lat + " lon: " + gps.state.lon);
-  }
-});
-
-parser.on("data", function(data) {
-  gps.update(data);
 });
