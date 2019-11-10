e3f633255e91ba016c9e93e9cd7da2ee59631ab8
benhsampson <bensampsondev@gmail.com>
2019-11-10 16:56:34 +1100

    simplified GPS server

--- src/gpsServer.js
+++ src/gpsServer.js
@@ -1,11 +1,10 @@
-// const io = require("socket.io"); //NEW
 const SerialPort = require("serialport");
 const parsers = SerialPort.parsers;
 
 const parser = new parsers.Readline({ delimiter: "\r\n" });
+const port = new SerialPort("/dev/ttyS0", { baudRate: 9600 });
 
-// const port = new SerialPort("/dev/ttyS0", { baudRate: 9600 });
-// port.pipe(parser);
+port.pipe(parser);
 
 var GPS = require("gps");
 var gps = new GPS();
@@ -20,14 +19,36 @@ parser.on("data", function(data) {
   gps.update(data);
 });
 
-//NEW
-const port = 8000;
-var io = require("socket.io")();
+// // const io = require("socket.io"); //NEW
+// const SerialPort = require("serialport");
+// const parsers = SerialPort.parsers;
 
-io.on("connection", client => {
-  io.emit("welcome");
-  // here you can start emitting events to the client
-});
+// const parser = new parsers.Readline({ delimiter: "\r\n" });
 
-io.listen(port);
-console.log("listening on port ", port);
+// // const port = new SerialPort("/dev/ttyS0", { baudRate: 9600 });
+// // port.pipe(parser);
+
+// var GPS = require("gps");
+// var gps = new GPS();
+
+// gps.on("data", function() {
+//   if (gps.state.lat !== null) {
+//     console.log("lat: " + gps.state.lat + " lon: " + gps.state.lon);
+//   }
+// });
+
+// parser.on("data", function(data) {
+//   gps.update(data);
+// });
+
+// //NEW
+// const port = 8000;
+// var io = require("socket.io")();
+
+// io.on("connection", client => {
+//   io.emit("welcome");
+//   // here you can start emitting events to the client
+// });
+
+// io.listen(port);
+// console.log("listening on port ", port);
