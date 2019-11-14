1a5f2fd39136a109bbd5848c2173d312b4e143b3
benhsampson <bensampsondev@gmail.com>
2019-11-14 23:23:11 +1100

    added geolib

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "./App.css";
 import Coordinates from "./Components/Coordinates";
 // import { subscribeToTimer } from "./api";
 import { subscribeToGPS } from "./api";
+// import { decimalToSexagesimal } from "geolib";
+import * as geolib from "geolib";
 
 function App() {
   // const [timestamp, setTimestamp] = useState("no timestamp yet");
   const [coordinates, setCoordinates] = useState("no coord yet");
   const [latitude, setLatitude] = useState("no coord yet");
   const [longitude, setLongitude] = useState("no coord yet");
+  // const [nearest, setNearest] = useState("na");
 
   // subscribeToTimer((err, timestamp) => setTimestamp(timestamp));
   subscribeToGPS((err, data) => {
@@ -19,6 +22,18 @@ function App() {
   }); //here
   console.log(latitude);
 
+  const stations = [
+    { name: "A", longitude: 146.55, latitude: -37.22 },
+    { name: "B", longitude: 145.17, latitude: -37.86 },
+    { name: "C", longitude: 144.32, latitude: -37.99 }
+  ];
+  const listStations = stations.map(station => (
+    <li key={station.name}>
+      {station.name} {station.longitude} {station.latitude}
+    </li>
+  ));
+
+  const nearest = coords => geolib.findNearest(coords, stations);
   return (
     <div className="App">
       <header className="App-header">
@@ -32,11 +47,16 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {listStations}{" "}
         </a>
-        <p className="App-intro">Latitude: {latitude}</p>
-        <p className="App-intro">Longitude: {longitude}</p>
+        <p className="App-intro">
+          Latitude: {geolib.decimalToSexagesimal(latitude)}
+        </p>
+        <p className="App-intro">
+          Longitude: {geolib.decimalToSexagesimal(longitude)}
+        </p>
         <Coordinates />
+        <a>nearest: {nearest} </a>
       </header>
     </div>
   );
