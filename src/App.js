fc49eabebf7d1b8bec9c0e07e10c395d306ed615
benhsampson <bensampsondev@gmail.com>
2020-02-02 22:02:24 +1100

    moved code to NearestTower component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
+import NearestTower from "./Components/NearestTower.js";
 // import Coordinates from "./Components/Coordinates";
 // import Dashboard from "./Components/dashboard";
 // import { subscribeToTimer } from "./api";
@@ -10,53 +11,51 @@ import { subscribeToGPS } from "./api";
 import * as geolib from "geolib";
 
 function App() {
-  // const [timestamp, setTimestamp] = useState("no timestamp yet");
-  // const [coordinates, setCoordinates] = useState("no coord yet");
-  const [latitude, setLatitude] = useState("no coord yet");
-  const [longitude, setLongitude] = useState("no coord yet");
-  // const [nearest, setNearest] = useState("na");
-
-  // subscribeToTimer((err, timestamp) => setTimestamp(timestamp));
-  subscribeToGPS((err, data) => {
-    setLatitude(data.lat);
-    setLongitude(data.lon);
-    console.log(data);
-  }); //here
-  console.log(latitude);
-
-  const stations = [
-    { name: "A", longitude: 145.1999, latitude: -37.8888 },
-    { name: "B", longitude: 145.1777, latitude: -37.8666 },
-    { name: "C", longitude: 145.1555, latitude: -37.8444 }
-  ];
-  const listStations = stations.map(station => (
-    <li key={station.name}>
-      {station.name} {station.longitude} {station.latitude}
-      {"    "}
-    </li>
-  ));
-
-  const nearest = coords => geolib.findNearest(coords, stations);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Station coordinates.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {listStations}{" "}
-        </a>
-        <a>-</a>
-        <a className="App-intro">Latitude: {Number(latitude).toFixed(6)}</a>
-        <a className="App-intro">Longitude: {Number(longitude).toFixed(6)}</a>
-        <a>nearest: {nearest} </a>
-      </header>
-    </div>
-  );
+  <NearestTower />;
+  // // const [timestamp, setTimestamp] = useState("no timestamp yet");
+  // // const [coordinates, setCoordinates] = useState("no coord yet");
+  // const [latitude, setLatitude] = useState("no coord yet");
+  // const [longitude, setLongitude] = useState("no coord yet");
+  // // const [nearest, setNearest] = useState("na");
+  // // subscribeToTimer((err, timestamp) => setTimestamp(timestamp));
+  // subscribeToGPS((err, data) => {
+  //   setLatitude(data.lat);
+  //   setLongitude(data.lon);
+  //   console.log(data);
+  // }); //here
+  // console.log(latitude);
+  // const stations = [
+  //   { name: "A", longitude: 145.1999, latitude: -37.8888 },
+  //   { name: "B", longitude: 145.1777, latitude: -37.8666 },
+  //   { name: "C", longitude: 145.1555, latitude: -37.8444 }
+  // ];
+  // const listStations = stations.map(station => (
+  //   <li key={station.name}>
+  //     {station.name} {station.longitude} {station.latitude}
+  //     {"    "}
+  //   </li>
+  // ));
+  // const nearest = coords => geolib.findNearest(coords, stations);
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>Station coordinates.</p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         {listStations}{" "}
+  //       </a>
+  //       <a>-</a>
+  //       <a className="App-intro">Latitude: {Number(latitude).toFixed(6)}</a>
+  //       <a className="App-intro">Longitude: {Number(longitude).toFixed(6)}</a>
+  //       <a>nearest: {nearest} </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/Components/NearestTower.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+
+const NearestTower = () => {
+  // const [timestamp, setTimestamp] = useState("no timestamp yet");
+  // const [coordinates, setCoordinates] = useState("no coord yet");
+  const [latitude, setLatitude] = useState("no coord yet");
+  const [longitude, setLongitude] = useState("no coord yet");
+  // const [nearest, setNearest] = useState("na");
+
+  // subscribeToTimer((err, timestamp) => setTimestamp(timestamp));
+  subscribeToGPS((err, data) => {
+    setLatitude(data.lat);
+    setLongitude(data.lon);
+    console.log(data);
+  }); //here
+  console.log(latitude);
+
+  const stations = [
+    {
+      name: "A",
+      longitude: 145.1999,
+      latitude: -37.8888
+    },
+    {
+      name: "B",
+      longitude: 145.1777,
+      latitude: -37.8666
+    },
+    {
+      name: "C",
+      longitude: 145.1555,
+      latitude: -37.8444
+    }
+  ];
+  const listStations = stations.map(station => (
+    <li key={station.name}>
+      {station.name} {station.longitude} {station.latitude}
+      {"    "}
+    </li>
+  ));
+
+  const nearest = coords => geolib.findNearest(coords, stations);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Station coordinates.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {listStations}{" "}
+        </a>
+        <a>-</a>
+        <a className="App-intro">Latitude: {Number(latitude).toFixed(6)}</a>
+        <a className="App-intro">Longitude: {Number(longitude).toFixed(6)}</a>
+        <a>nearest: {nearest} </a>
+      </header>
+    </div>
+  );
+};
+
+export default NearestTower;
